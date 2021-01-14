6b2db634e1ec7f676f43f6380c4f03d1997fdd61
Erin Burt <tech@thehiveworks.com>
2021-01-14 14:26:17 -0600

    Updated update checker to pull from HTTPS version of site.

--- comiccontrol/parts/update-check.php
+++ comiccontrol/parts/update-check.php
@@ -13,7 +13,7 @@ if($ccuser->authlevel != 2){
 	echo '<div class="msg error f-c">' . $lang['You do not have permission to access this page.'] . '</div>';
 }else{
 //get the newest version from the CC server
-$version = get_info("http://www.comicctrl.com/version-control/getversion.php");
+$version = get_info("https://www.comicctrl.com/version-control/getversion.php");
 $query = "SELECT * FROM cc_" . $tableprefix . "options WHERE optionname='version' LIMIT 1";
 $stmt = $cc->prepare($query);
 $stmt->execute();
@@ -34,9 +34,9 @@ else{
 	$stmt = $cc->prepare($query);
 	$stmt->execute(['version' =>$version,'option'=>'newestversion']);
 	
-	get_file("http://www.comicctrl.com/version-control/upgradescripts/upgrade.txt",dirname(__FILE__) . '/upgrade.php');
+	get_file("https://www.comicctrl.com/version-control/upgradescripts/upgrade.txt",dirname(__FILE__) . '/upgrade.php');
 	echo '<p>' . $lang['Your version of ComicControl needs updating!  Here are change notes for this update:'] . '</p>';
-	$versionnotes = get_info("http://www.comicctrl.com/version-control/versionnotes.php?version=" . $currentversion);
+	$versionnotes = get_info("https://www.comicctrl.com/version-control/versionnotes.php?version=" . $currentversion);
 	echo '<p>' . $versionnotes . '</p>';
 	echo '<p>' . $lang['Please click the button below to update your version of ComicControl.  After clicking the button, please do not leave the page until you receive confirmation that ComicControl has been updated.  Please be aware that this will overwrite the main ComicControl files.'] . '</p>';
 	echo '<div class="cc-btn-row">';
