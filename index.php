5b0608eb8a1d38ec3fba616b132694b02e3eee95
Erin Burt <tech@thehiveworks.com>
2021-02-03 13:32:46 -0600

    Added link to ComicControl site to basic template.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 /* 
 COMICCONTROL
-Version 4.2.4
-1/21/2021
+Version 4.2.5
+2/3/2021
 Built by Erin Burt with help from others.
 Copyright 2012-2021 Erin Burt.
 See comicctrl.com/thanks for contributors and included third-party MIT-licensed packages.
--- templates/basic/includes/footer.php
+++ templates/basic/includes/footer.php
@@ -1,4 +1,7 @@
     </main>
 </div>
+<footer id="cc-footer">
+This site is powered by <a href="https://www.comicctrl.com/">ComicControl</a>.
+</footer>
 </body>
 </html>
