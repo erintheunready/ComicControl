f10d60013aeda20eaa125010a7196ee737505594
Erin Burt <tech@thehiveworks.com>
2021-02-03 13:17:09 -0600

    Fixed bug in basic template with include pointing to wrong file location.

--- templates/basic/page.php
+++ templates/basic/page.php
@@ -1,6 +1,6 @@
 <?php
-include('templates/main/includes/header.php'); ?>
+include('templates/basic/includes/header.php'); ?>
 
 <?php $ccpage->module->display(); ?>
 
-<?php include('templates/main/includes/footer.php'); ?>
+<?php include('templates/basic/includes/footer.php'); ?>
